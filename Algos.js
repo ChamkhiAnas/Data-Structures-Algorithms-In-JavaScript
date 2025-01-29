0eeda21b9fa6d01028094133c83c65a562e9667f
ChamkhiAnas <mahmoudbennouna@gmail.com>
2025-01-29 13:37:31 +0100

    FizzBuzz

--- Algos.js
+++ Algos.js
@@ -27,5 +27,23 @@ function SentenceCapitalization(text){
 }
 
 
+function fizzBuzz(n){
+    for(let i=1;i<=n;i++){
+        if(i%3==0 &&  i%5==0){
+            console.log('FizzBuzz')
+        }
+        else if(i%3==0){
+            console.log('Fizz')
+        }
+        else if(i%5==0){
+            console.log('Buzz')
+        }
+        else {
+            console.log(i)
+        }
 
-console.log(SentenceCapitalization("subcomandante marocs "))
+    }
+}
+
+
+console.log(fizzBuzz(8))
